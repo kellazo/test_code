20fd2070710e364e4dac0b6b586ed61bbdb25caa
kellazo <gilli11519@gmail.com>
2015-02-13 12:01:12 +0100

    snipets

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, char** argv) // unsign sense signe  tipus basics de C i rangs de valors
 //quan ocupa un punter depen del tamañ de la memoria dreccionable. de la arquitectura si es 32 o 64 bytes.
@@ -12,19 +13,20 @@ int main(int argc, char** argv) // unsign sense signe  tipus basics de C i rangs
 //com fas varies cadenes doncs amb un char* com guardes una llista de char* amb char**
 // es com char argc[ò][ò]; i char argc[`1][`0]; es com una taula.
 {
-	//char c[10] = "hola";
+	char c[10] = "hola";
 	//char cc = 'R';
 	//printf("%s", &cc);
 
-	char c[0] = 'h';
+	/*char c[0] = 'h';
 	char c[1] = 'o';
 	char c[2] = 'l';
 	char c[3] = 'a';
-	char c[4] = '\0';
+	char c[4] = '\0';*/
 	printf("%s", &c[1]);
 	//char*pc = &c;
 	//printf("%s", pc);
 	//printf("%s", c);
-	getchar();
+	//getchar();
 	//return 0;
 }
+//code snippets manager visual studio
