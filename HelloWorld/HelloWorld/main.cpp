92ceea31f1914bad1ee140e17f11717243da95ea
kellazo <gilli11519@gmail.com>
2015-02-13 11:05:36 +0100

    first commit visual

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) // unsign sense signe  tipus basics de C i rangs de valors
+//quan ocupa un punter depen del tamañ de la memoria dreccionable. de la arquitectura si es 32 o 64 bytes.
+//un char ocupa un byte
+
+//punter es un char* es una direccio d ememoria que apunta a algun lloc
+// un char** es un punter que apunta a un punter que et troba una direccio de memoria
+//perque ho fem servir? 
+//argc conte un 1 el primer element duna llista darguments es el primer programa que sexecuta
+//argv conte un arrays de arrays de chars, conte els arguments de varis arguments amb char
+//com fas varies cadenes doncs amb un char* com guardes una llista de char* amb char**
+// es com char argc[ò][ò]; i char argc[`1][`0]; es com una taula.
+{
+	char c[10] = "hola";
+	char cc = 'R';
+	printf("%s", &cc);
+
+	c[0] = 'h';
+	c[1] = 'o';
+	c[2] = 'l';
+	c[3] = 'a';
+	c[4] = '\0';
+	printf("%s", &c[1]);
+	//char*pc = &c;
+	//printf("%s", pc);
+	//printf("%s", c);
+	getchar();
+	//return 0;
+}
